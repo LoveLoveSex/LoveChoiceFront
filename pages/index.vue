19a3fcd94089503dee4b61075196d67c8accc6fa
JpnLavender <irimamekun@gmail.com>
2017-10-12 23:42:25 +0900

    サーバーからデータを持ってきて表示できるように

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
 <template>
 	<div>
-		<h1>
-			HelloWorld
-		</h1>
+		<input type="text" v-model="search" v-on:keyup.enter="submit"><br>
+		<br>
+		検索ヒット数: {{hotels.length}}
+		<ul>
+			<li v-for="hotel in hotels">{{hotel.name}}</li>
+		</ul>
+
 	</div>
 </template>
+
+<script>
+import axios from 'axios'
+export default{
+	data(){
+		return {
+			search: "",
+			hotels: []
+		}
+	},
+	methods: {
+		submit: function(){
+			var self = this
+			axios.get(`http://localhost:4567/hotels/${this.search}`).then(function(res){
+				console.log(res.data)
+				self.hotels = res.data
+			})
+		}
+	}
+}
+</script>
